bd28ff001a97176aa2f72cc7cde1c2d77d4863a7
PankajSinghSheoran <pankajsheoran728@gmail.com>
2023-09-06 20:43:59 +0530

    Created Header, Button and Tasks components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 import './App.css';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+    {
+      id: 4,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+  ]);
   return (
     <>
       <div>
         <Header title="Task Manager" />
+        <Tasks tasks={tasks} />
       </div>
     </>
   );
